068f02fa3dd62f008b89a76d3c7b5380fc5e3f66
Josep <apuratepp@gmail.com>
2020-11-02 13:43:36 +0100

    Fetch dependencies from git

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule BroadwayKafka.MixProject do
   defp deps do
     [
       {:broadway, "~> 0.6.0"},
-      {:brod, path: "/Users/apuratepp/Developer/code/github/klarna/brod", override: true},
+      {:brod, git: "git@github.com:apuratepp/brod.git", branch: "oauthbearer"},
       {:ex_doc, ">= 0.19.0", only: :docs}
     ]
   end
